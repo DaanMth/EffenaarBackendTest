92ab935b4d6f9dce709a6728982d1d6eb10c8ae9
Tom Derks <459530@student.fontys.nl>
2022-05-20 11:33:22 +0200

    commit

--- EffenaarTest.Model/achievementModel.cs
+++ EffenaarTest.Model/achievementModel.cs
@@ -1,9 +1,13 @@
 ﻿using System.Buffers.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace EffenaarTest.Model
 {
     public class achievementModel
     {
+        [BsonId]
+        [BsonRepresentation((BsonType.ObjectId))]
         public string Name { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
