1d4e4105088ee368181c4f5d01943dc4e4350cbc
Matheeuwsen,Daan D.W <d.matheeuwsen@student.fontys.nl>
2022-05-23 09:46:17 +0200

    Adding Account Controller

---
+++ EffenaarTest/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using EffenaarTest.Data;
+using EffenaarTest.Logic;
+using EffenaarTest.Model;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodooBackend.Api.Controllers
+{
+    [EnableCors("AllowCORS")]
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly AccountLogic _accountLogic;
+        
+        public AccountController(FoodooContext context)
+        {
+            _accountLogic = new AccountLogic(new AccountData(context));
+        }
+
+        [HttpPost("/account/register")]
+        public void Register(ApiAccount account)
+        {
+            _accountLogic.Register(account);
+        }
+
+        [HttpPost("/account/login")]
+        public string Login(ApiAccount account)
+        {
+            return _accountLogic.Login(account);
+        }
+
+        [HttpGet("/account/info/{token}")]
+        public IActionResult GetAccountInfo(string token)
+        {
+            return StatusCode(200, AuthenticationLogic.getAccountInfo(token));
+            
+        }
+
+        [HttpPost("/googleauth/{token}")]
+        public async Task<ActionResult<Account>> GoogleLogin(string token)
+        {
+            Account account = await _accountLogic.GoogleAuth(token);
+            return account;
+        }
+    }
+}
